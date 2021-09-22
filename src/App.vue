fa37bcdc1865e7db16beed0f8b4769641336c56e
unknown <younas.h03@htlwienwest.at>
2021-09-22 14:01:44 +0200

    Aufgabe 2 done

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar class="grey lighten-5" height="74" app>
+      <img class="mx-2" src="images/art.jpg" style="height:70px;" />
+      <span class="text-truncate text-h4 blue-grey--text darken-1" style="font-family: Poppins"
+        >Haroon Younas</span
+      >
+      <v-spacer></v-spacer>
+      
+      <v-btn class="grey accent-4" active-class="yellow accent-4" to="/" exact elevation="2" large text
+        >Home</v-btn
+      >
+      <v-btn class="grey accent-4" active-class="yellow accent-4" to="/products" exact elevation="2" large text style="margin-left: 5px;"
+        >Products</v-btn
+      >
+      <v-btn class="grey accent-4" active-class="yellow accent-4" to="/about" exact elevation="2" large text style="margin-left: 5px;"
+        >About</v-btn
+      >
+    </v-app-bar>
+
+    <v-main>
+      <div class="d-flex">
+        <div style="width:250px"></div>
+        <div class="mt-10" style="flex:1"><router-view></router-view></div>
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
+a:not(active-class) {
+  color: white !important;
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-app>
+    <h1 class="font-weight-black" style="margin-left: 100px;">About</h1>
+    <p style="margin-left: 100px;">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, </p>
+  </v-app>
 </template>
